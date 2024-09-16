3b92180eaeafba413df2e4e59bd8dfc06b0d9b35
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-09-16 10:23:44 -0300

    Changing the parameters

--- geneticos.py
+++ geneticos.py
@@ -1,74 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Função de Rastrigin
 def rastrigin(X):
     A = 10
     n = len(X)
     return A * n + sum([(x**2 - A * np.cos(2 * np.pi * x)) for x in X])
-
-# # Algoritmo Genético
-# class GeneticAlgorithm:
-#     def __init__(self, pop_size, num_generations, mutation_rate, crossover_rate, dimension, bounds):
-#         self.pop_size = pop_size
-#         self.num_generations = num_generations
-#         self.mutation_rate = mutation_rate
-#         self.crossover_rate = crossover_rate
-#         self.dimension = dimension
-#         self.bounds = bounds
-#         self.population = self.initialize_population()
-
-#     def initialize_population(self):
-#         return np.random.uniform(self.bounds[0], self.bounds[1], (self.pop_size, self.dimension))
-
-#     def evaluate(self, population):
-#         return np.array([rastrigin(individual) for individual in population])
-
-#     def select_parents(self, fitness):
-#         indices = np.argsort(fitness)
-#         return self.population[indices[:2]]  # Seleciona os 2 melhores
-
-#     def crossover(self, parent1, parent2):
-#         if np.random.rand() < self.crossover_rate:
-#             point = np.random.randint(1, self.dimension - 1)
-#             return np.concatenate((parent1[:point], parent2[point:]))
-#         return parent1
-
-#     def mutate(self, individual):
-#         for i in range(self.dimension):
-#             if np.random.rand() < self.mutation_rate:
-#                 individual[i] += np.random.uniform(-0.5, 0.5)
-#         return individual
-
-#     def evolve(self):
-#         for generation in range(self.num_generations):
-#             fitness = self.evaluate(self.population)
-#             new_population = []
-
-#             for _ in range(self.pop_size // 2):  # Gerar nova população
-#                 parents = self.select_parents(fitness)
-#                 child1 = self.mutate(self.crossover(parents[0], parents[1]))
-#                 child2 = self.mutate(self.crossover(parents[1], parents[0]))
-#                 new_population.extend([child1, child2])
-
-#             self.population = np.array(new_population)
-
-#             best_fitness = np.min(fitness)
-#             best_individual = self.population[np.argmin(fitness)]
-#             print(f"Geração {generation}: Melhor valor da função = {best_fitness}, Melhor indivíduo = {best_individual}")
-
-# # Parâmetros
-# pop_size = 100
-# num_generations = 200
-# mutation_rate = 0.1
-# crossover_rate = 0.7
-# dimension = 10
-# bounds = (-5.12, 5.12)  # Limites típicos para a função de Rastrigin
-
-# # Executar Algoritmo Genético
-# ga = GeneticAlgorithm(pop_size, num_generations, mutation_rate, crossover_rate, dimension, bounds)
-# ga.evolve()
 
 # Algoritmo Genético
 class GeneticAlgorithm:
@@ -138,11 +75,11 @@ class GeneticAlgorithm:
 
 # Parâmetros
 pop_size = 100
-num_generations = 100
+num_generations = 200
 mutation_rate = 0.1
 crossover_rate = 0.7
-dimension = 10
-bounds = (-5.12, 5.12)  # Limites típicos para a função de Rastrigin
+dimension = 20
+bounds = (-10, 10)  # Limites típicos para a função de Rastrigin
 
 # Executar Algoritmo Genético
 ga = GeneticAlgorithm(pop_size, num_generations, mutation_rate, crossover_rate, dimension, bounds)
